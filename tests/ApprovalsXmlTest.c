4ff89766771cd70ebbb522b828a8570721027b1f
Peter Kofler <peter.kofler@code-cop.org>
2020-05-15 19:34:17 +0200

    Red: test_format_single_intent

--- tests/ApprovalsXmlTest.c
+++ tests/ApprovalsXmlTest.c
@@ -18,10 +18,22 @@ static void test_format_xml_null(void** state)
     free((void*)formatted);
 }
 
+static void test_format_single_intent(void** state)
+{
+    (void)state; /* unused */
+
+    const char* xml = "<a>foo</a>";
+    const char* formatted = __approvals_xml_format(xml);
+
+    assert_string_equal("<a>\n  foo\n</a>", formatted);
+    free((void*)formatted);
+}
+
 int main(void)
 {
     const struct CMUnitTest test_suite[] = {
-        cmocka_unit_test(test_format_xml_null), /* */
+        cmocka_unit_test(test_format_xml_null),      /* */
+        cmocka_unit_test(test_format_single_intent), /* */
     };
 
     return cmocka_run_group_tests(test_suite, NULL, NULL);
