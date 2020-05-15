3515d30e47266856f3cb49f3d0ec3fca90beee25
Peter Kofler <peter.kofler@code-cop.org>
2020-05-15 21:24:51 +0200

    Delete duplicated tests.

--- tests/ApprovalsXmlTest.c
+++ tests/ApprovalsXmlTest.c
@@ -20,9 +20,16 @@ typedef struct {
 } FormatterTestCase;
 
 FormatterTestCase test_cases[] = {
-    {"<nope />", "<nope />\n"},                                   /* empty */
-    {"<a>foo</a>", "<a>\n  foo\n</a>\n"},                         /* intent */
-    {"<a><b>foo</b></a>", "<a>\n  <b>\n    foo\n  </b>\n</a>\n"}, /* nested */
+    {"<nope />", /* empty */
+     "<nope />\n"},
+    {"<a>foo</a>", /* intent */
+     "<a>\n  foo\n</a>\n"},
+    {"<a><b>foo</b></a>", /* multi */
+     "<a>\n  <b>\n    foo\n  </b>\n</a>\n"},
+    {"<r><a>bar</a><b>foo</b></r>", /* sibling */
+     "<r>\n  <a>\n    bar\n  </a>\n  <b>\n    foo\n  </b>\n</r>\n"},
+    {"<r><a>bar</a>foo</r>", /* text */
+     "<r>\n  <a>\n    bar\n  </a>\n  foo\n</r>\n"},
 };
 
 static void test_format_xml(void** state)
@@ -36,71 +43,15 @@ static void test_format_xml(void** state)
     free((void*)formatted);
 }
 
-static void test_format_xml_null(void** state)
-{
-    (void)state; /* unused */
-
-    const char* xml = "<nope />";
-    const char* formatted = __approvals_xml_format(xml);
-
-    assert_string_equal("<nope />\n", formatted);
-    free((void*)formatted);
-}
-
-static void test_format_single_intent(void** state)
-{
-    (void)state; /* unused */
-
-    const char* xml = "<a>foo</a>";
-    const char* formatted = __approvals_xml_format(xml);
-
-    assert_string_equal("<a>\n  foo\n</a>\n", formatted);
-    free((void*)formatted);
-}
-
-static void test_format_multi_intent(void** state)
-{
-    (void)state; /* unused */
-
-    const char* xml = "<a><b>foo</b></a>";
-    const char* formatted = __approvals_xml_format(xml);
-
-    assert_string_equal("<a>\n  <b>\n    foo\n  </b>\n</a>\n", formatted);
-    free((void*)formatted);
-}
-
-static void test_format_sibling_intent(void** state)
-{
-    (void)state; /* unused */
-
-    const char* xml = "<r><a>bar</a><b>foo</b></r>";
-    const char* formatted = __approvals_xml_format(xml);
-
-    assert_string_equal("<r>\n  <a>\n    bar\n  </a>\n  <b>\n    foo\n  </b>\n</r>\n", formatted);
-    free((void*)formatted);
-}
-
-static void test_format_sibling_text(void** state)
-{
-    (void)state; /* unused */
-
-    const char* xml = "<r><a>bar</a>foo</r>";
-    const char* formatted = __approvals_xml_format(xml);
-
-    assert_string_equal("<r>\n  <a>\n    bar\n  </a>\n  foo\n</r>\n", formatted);
-    free((void*)formatted);
-}
-
 int main(void)
 {
     const struct CMUnitTest test_suite[] = {
         cmocka_unit_test(test_verify_xml),                          /* */
         cmocka_unit_test_prestate(test_format_xml, &test_cases[0]), /* */
-        cmocka_unit_test(test_format_xml_null),                     /* */
-        cmocka_unit_test(test_format_single_intent),                /* */
-        cmocka_unit_test(test_format_multi_intent),                 /* */
-        cmocka_unit_test(test_format_sibling_intent),               /* */
-        cmocka_unit_test(test_format_sibling_text),                 /* */
+        cmocka_unit_test_prestate(test_format_xml, &test_cases[1]), /* */
+        cmocka_unit_test_prestate(test_format_xml, &test_cases[2]), /* */
+        cmocka_unit_test_prestate(test_format_xml, &test_cases[3]), /* */
+        cmocka_unit_test_prestate(test_format_xml, &test_cases[4]), /* */
     };
 
     return cmocka_run_group_tests(test_suite, NULL, NULL);
