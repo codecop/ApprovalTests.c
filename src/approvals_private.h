3f6fa702ddf24690a34c21238c12a1d280617766
Peter Kofler <peter.kofler@code-cop.org>
2020-10-04 21:31:14 +0200

    Fix duplicate const declaration specifier.

--- src/approvals_private.h
+++ src/approvals_private.h
@@ -10,6 +10,6 @@ extern const char* approvals_file_name_for(const char* full_file_name,
                                            bool is_approved,
                                            const char* extension_no_dot);
 
-extern const const char* approvals_load(const char* filename);
+extern const char* approvals_load(const char* filename);
 extern void approvals_save(const char* filename, const char* data);
 extern void approvals_delete(const char* filename);
