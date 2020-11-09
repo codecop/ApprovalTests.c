7bff428c0d31f6269ce680914a7101c769a7b328
Peter Kofler <peter.kofler@code-cop.org>
2020-11-09 23:46:40 +0100

    Add manual download of latest GitHub built to release.

--- release.bat
+++ release.bat
@@ -1,3 +1,4 @@
+@rem create Windows x86 binaries
 setlocal
 call ..\setenv.bat
 call make clean_all
@@ -6,6 +7,7 @@ call make build example
 call ant -Darch=x86
 endlocal
 
+@rem create Windows x64 binaries
 setlocal
 call ..\setenv_x64.bat
 call make clean_all
@@ -16,5 +18,9 @@ call ant
 @rem leave x64 binaries around
 make clean
 endlocal
+
+@rem get Linux x64 binaries
+start "GitHub" https://github.com/codecop/ApprovalTests.c/actions
+@echo "download artefact"
 
 @pause
