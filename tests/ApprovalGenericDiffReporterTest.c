533112878072a64352bdf1c9f969b5754dbfbee9
Peter Kofler <peter.kofler@code-cop.org>
2020-11-16 00:49:03 +0100

    Only run kdiff test when it is available.

--- tests/ApprovalGenericDiffReporterTest.c
+++ tests/ApprovalGenericDiffReporterTest.c
@@ -3,19 +3,34 @@
 #include <stddef.h> /* used by cmocka */
 
 #include <cmocka.h>
+#include <stdio.h>
 
 #include "../src/approval_failure_reporter.h"
 #include "../src/approval_generic_diff_reporter.h"
+#include "../src/file_utils.h"
+
+static int has_file(const char* file, const char* test)
+{
+    if (approvals_file_exists(file)) {
+        return 1;
+    }
+
+    fprintf(stderr, "Ignoring test %s, test file %s missing.\n", test, file);
+    return 0;
+}
 
 static void show_windows_diff_reporter(void** state)
 {
     (void)state; /* unused */
+    if (!has_file(WINDOWS_KDIFF3.parameters, __func__)) {
+        return;
+    }
 
     approvals_use_reporter(approval_report_failure_generic_diff(WINDOWS_KDIFF3));
 
-    approval_report_failure((struct ApprovalFileNames){
-        "tests/ApprovalGenericDiffReporterTest_approved.txt",
-        "tests/ApprovalGenericDiffReporterTest_received.txt"});
+    approval_report_failure(
+        (struct ApprovalFileNames){"tests/ApprovalGenericDiffReporterTest_approved.txt",
+                                   "tests/ApprovalGenericDiffReporterTest_received.txt"});
 }
 
 static int reset_reporters(void** state)
