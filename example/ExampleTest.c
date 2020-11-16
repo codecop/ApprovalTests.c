0c42618a2462246cb233d42d877b2e727836e352
Peter Kofler <peter.kofler@code-cop.org>
2020-11-16 10:12:06 +0100

    Add text example.

--- example/ExampleTest.c
+++ example/ExampleTest.c
@@ -2,21 +2,30 @@
 #include <stdarg.h> /* used by cmocka */
 #include <stddef.h> /* used by cmocka */
 
+#include <cmocka.h> /* see https://cmocka.org/ */
+
 #include <approvals_cmocka.h> /* see https://github.com/codecop/ApprovalTests.c */
-#include <cmocka.h>           /* see https://cmocka.org/ */
 #include <stdlib.h>           /* used by approvals */
+
+static void test_some_text(void** state)
+{
+    (void)state; /* unused */
+
+    verify_txt("text");
+}
 
 static void test_some_xml(void** state)
 {
     (void)state; /* unused */
 
-    verify_xml("<nope />");
+    verify_xml("<xml />");
 }
 
 int main(void)
 {
     const struct CMUnitTest test_suite[] = {
-        cmocka_unit_test(test_some_xml), /* */
+        cmocka_unit_test(test_some_text), /* */
+        cmocka_unit_test(test_some_xml),  /* */
     };
 
     return cmocka_run_group_tests(test_suite, NULL, NULL);
