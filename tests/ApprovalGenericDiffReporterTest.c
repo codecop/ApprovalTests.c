9a4c49e69fc28f18c026f913a554ea4af17663eb
Peter Kofler <peter.kofler@code-cop.org>
2020-11-20 20:29:56 +0100

    Check if necessary executable is here.

--- tests/ApprovalGenericDiffReporterTest.c
+++ tests/ApprovalGenericDiffReporterTest.c
@@ -3,9 +3,26 @@
 #include <stddef.h> /* used by cmocka */
 
 #include <cmocka.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #include "../src/approval_failure_reporter.h"
 #include "../src/approval_generic_diff_reporter.h"
+#include "../src/file_utils.h"
+
+/* My local installations */
+const char* kdiff3 = "C:\\Program Files (x86)\\KDiff3\\kdiff3.exe";
+
+static bool has_file(const char* file, const char* test)
+{
+    if (approvals_file_exists(file)) {
+        return true;
+    }
+
+    fprintf(stderr, "Ignoring test %s, test file %s missing.\n", test, file);
+    return false;
+    /* TODO duplication, extract test helper to include directly */
+}
 
 /* show is not testing - check test output */
 static void show_windows_diff_reporter(void** state)
@@ -32,6 +49,9 @@ static void test_find_no_working_reporter(void** state)
 static void test_find_first_working_reporter(void** state)
 {
     (void)state; /* unused */
+    if (!has_file(kdiff3, __func__)) {
+        return;
+    }
 
     struct DiffInfo* all_diffs = (struct DiffInfo*)&WINDOWS_DIFFS;
     struct DiffInfo* diff = approval_first_working_diff(all_diffs);
