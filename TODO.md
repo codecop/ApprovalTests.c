7a3e406655974f443ec90cdc15c4843189e36fb8
Peter Kofler <peter.kofler@code-cop.org>
2020-11-21 16:19:33 +0100

    Use old server to create x86 Linux releases.

--- TODO.md
+++ TODO.md
@@ -1,16 +1,13 @@
 # Open
 
-* test resolving a linux tool, should work in ubuntu-latest
 * #ifndef __approval_assert_equals add a plain assert reporter by default
 
 * Documentation
 
   * describe features related to usual approvals:
-  * verify text, verify xml, quiet reporter, all diff reporters, cmocka integration
+  * verify text, verify xml, cmocka integration
   * binaries for linux and windows x64
+  * 1.1 quiet reporter, diff reporters,
+  * binaries for linux and windows x86
 
 * create second Example with reporters
-
-## Ideas
-
-* use x86 ubuntu server to test it
--- release.bat
+++ release.bat
@@ -1,26 +1,32 @@
+@set V=1.1
+@rem rmdir /S /Q releases\v%V%
+mkdir releases\v%V%
+
 @rem create Windows x86 binaries
-setlocal
+@setlocal
 call ..\setenv.bat
-call make clean_all
-call test
-call make build example
-call ant -Darch=x86
-endlocal
+call make build test example
+call ant -Darch=x86 -Dversion=%V%
+@endlocal
 
 @rem create Windows x64 binaries
-setlocal
+@setlocal
 call ..\setenv_x64.bat
-call make clean_all
-call test
-call make build example
-call ant
-
+call make build test example
+call ant -Dversion=%V%
 @rem leave x64 binaries around
 make clean
-endlocal
+@endlocal
+
+@rem create Linux x86 binaries
+@setlocal
+set PATH=%PATH%;C:\Progra~2\msysGit-1.8.5\bin
+call ssh pkofler@anthill "cd ApprovalTests.c && git pull && make build test && ant -Dos=Linux -Darch=x86 -Dversion=%V%"
+call scp pkofler@anthill:~/ApprovalTests.c/releases/v%V%/Binaries_Linux_x86.zip releases\v%V%\
+@endlocal
 
 @rem get Linux x64 binaries
-start "GitHub" https://github.com/codecop/ApprovalTests.c/actions
-@echo "download artefact"
-
+start "GitHub" https://github.com/codecop/ApprovalTests.c/actions?query=workflow%3A%22CI+Workflow+C+Make%22
+@echo click latest job, download binaries
+explorer "releases\v%V%"
 @pause
