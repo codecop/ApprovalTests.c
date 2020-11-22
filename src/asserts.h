dde42e72fc60c41fbe99256fa4126b4af1803276
Peter Kofler <peter.kofler@code-cop.org>
2020-11-22 17:05:58 +0100

    Remove duplication.

--- src/asserts.h
+++ src/asserts.h
@@ -3,19 +3,15 @@
  * Copyright (c) 2020, Peter Kofler. All rights reserved.
  * BSD3 licensed.
  */
-#ifndef Asserts_H_
-#define Asserts_H_
+#ifndef Approval_Asserts_H_
+#define Approval_Asserts_H_
 
 #include <assert.h>
 
 #define assert_not_null(p) assert((p) != NULL);
 
-#define assert_str_not_empty(s) \
-    assert((s) != NULL);           \
-    assert(*(s) != '\0');
+#define assert_str_not_empty(s) assert_not_null(s) assert(*(s) != '\0');
 
-#define assert_str_longer_than(s, len) \
-    assert((s) != NULL);                  \
-    assert(strlen((s)) > (len));
+#define assert_str_longer_than(s, len) assert_not_null(s) assert(strlen((s)) > (len));
 
 #endif
